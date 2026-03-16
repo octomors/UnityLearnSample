0d6fc057be58e2c97215a3eb4f17c7a618f53093
dianakonushkina <konushkina.diana@gmail.com>
2026-03-16 13:51:13 +0700

    parts 1 and 2
    changing GameObjectActivator.cs, adding the prefab to LearnScene2; creating a copying SampleScript child, adding it to the Laba5 scene.

--- Assets/LearnMaterials 2/Scripts/GameObjectActivator.cs
+++ Assets/LearnMaterials 2/Scripts/GameObjectActivator.cs
@@ -7,7 +7,12 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1GP4_m0MzOF8L5t5pZxLChu3V_TFIq1czi1oJQ2X5kpU/edit?usp=sharing")]
 public class GameObjectActivator : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("Список объектов, которым будет переключаться состояние")]
     private List<StateContainer> targets;
+    [Space]
+    [SerializeField]
+    [Tooltip("Отрисовать связи с управляемыми объектами. Зелёная - будет включено. Красная - выключена.")]
     private bool debug;
 
     private void Awake()
@@ -17,6 +22,8 @@ public class GameObjectActivator : MonoBehaviour
             item.defaultValue = item.targetGO.activeSelf;
         }
     }
+    
+    [ContextMenu("ActivateModule")]
     public void ActivateModule()
     {
         SetStateForAll();
